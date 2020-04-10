70ac19ccdbdddf6b7b059cf26a06903eb2451747
y-kawamura <tetora.y@gmail.com>
2020-04-11 08:57:28 +0900

    Add register api

---
+++ tests/Feature/RegisterApiTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class RegisterApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testShouldCreateNewUser()
+    {
+        $data = [
+            'name' => 'user',
+            'email' => 'user@email.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234',
+        ];
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
+    }
+}
